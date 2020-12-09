891592bdc1ccb2f51c0ed1023d285d32d9076786
SunShine <sunshine@localhost.localdomain>
2020-12-08 19:00:32 -0500

    splash update and sw version change

--- SunBible_wallpaper_img/sw.js
+++ SunBible_wallpaper_img/sw.js
@@ -4,27 +4,39 @@
  *
  */
 
-const version = "0.1.00";
+const version = "0.1.01";
 const cacheName = 'img-${version}';
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(cacheName).then(cache => {
       return cache.addAll([
-        '/SunBible_IMG_Library/',
         '/SunBible_IMG_Library/SunBible_wallpaper_img/wallpaper.html',
         '/SunBible_IMG_Library/SunBible_wallpaper_img/app.html',
+
+        '/SunBible_IMG_Library/SunBible_wallpaper_img/css/app.css',
+        '/SunBible_IMG_Library/SunBible_wallpaper_img/css/splash.css',
+        '/SunBible_IMG_Library/SunBible_wallpaper_img/css/wallpaper.css',
+
         '/SunBible_IMG_Library/SunBible_wallpaper_img/img/W.png',
+        '/SunBible_IMG_Library/SunBible_wallpaper_img/img/wallpaper.png',
+
         '/SunBible_IMG_Library/SunBible_wallpaper_img/js/jquery-3.5.1.min.js',
+        '/SunBible_IMG_Library/SunBible_wallpaper_img/js/app.js',
+        
 
+        '/SunBible_IMG_Library/SunBible_wallpaper_img/img/Genesis27_28.png',
+        '/SunBible_IMG_Library/SunBible_wallpaper_img/img/Joshua24_15.png',
+        '/SunBible_IMG_Library/SunBible_wallpaper_img/img/1_Chronicles4_9-10.png',
+        '/SunBible_IMG_Library/SunBible_wallpaper_img/img/Isaiah40_28.png',
+        '/SunBible_IMG_Library/SunBible_wallpaper_img/img/John3_16.png',
 
-
-        'Psalm_Slides/Psalm1/Psalm1.png',
-        'Psalm_Slides/Psalm1/Psalm1_1.png',
-        'Psalm_Slides/Psalm1/Psalm1_2.png',
-        'Psalm_Slides/Psalm1/Psalm1_3.png',
-        'Psalm_Slides/Psalm1/Psalm1_4.png',
-        'Psalm_Slides/Psalm1/Psalm1_5.png',
-        'Psalm_Slides/Psalm1/Psalm1_6.png'
+        '/SunBible_IMG_Library/SunBible_wallpaper_img/Psalm_Slides/Psalm1/Psalm1.png',
+        '/SunBible_IMG_Library/SunBible_wallpaper_img/Psalm_Slides/Psalm1/Psalm1_1.png',
+        '/SunBible_IMG_Library/SunBible_wallpaper_img/Psalm_Slides/Psalm1/Psalm1_2.png',
+        '/SunBible_IMG_Library/SunBible_wallpaper_img/Psalm_Slides/Psalm1/Psalm1_3.png',
+        '/SunBible_IMG_Library/SunBible_wallpaper_img/Psalm_Slides/Psalm1/Psalm1_4.png',
+        '/SunBible_IMG_Library/SunBible_wallpaper_img/Psalm_Slides/Psalm1/Psalm1_5.png',
+        '/SunBible_IMG_Library/SunBible_wallpaper_img/Psalm_Slides/Psalm1/Psalm1_6.png'
 ])
           .then(() => self.skipWaiting());
     })
